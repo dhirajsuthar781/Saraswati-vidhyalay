c6a40531637913df80904072400df3eab19db6ed
dhiraj.earth <drsuthar781@gmail.com>
2024-12-07 13:52:33 +0530

    some bug

--- src/pages/home_page/Acadmic_calender.jsx
+++ src/pages/home_page/Acadmic_calender.jsx
@@ -18,19 +18,24 @@ export default function Acadmic_calender() {
         class_pencil={'absolute -bottom-2 -right-10'} />
 
       <div className=' px-24 w-full flex flex-row justify-between items-center'>
+        
         <div className=' flex flex-row justify-center items-center gap-4'>
           <span className=' font-Poppins text-[26px] font-semibold'>January</span>
           <Button className=' border-[1.5px] border-black rounded-xl'  variant="bordered" isIconOnly>
             <IoIosArrowDown className=' text-2xl'/>
           </Button>
         </div>
+        
         <p className='font-Poppins text-[26px] font-semibold'> Year, 2025</p>
+        
       </div>
+      
       <div className=' w-full mt-10 mb-8'>
         <Timeline/>
       </div>
 
       <Content />
+
     </div>
   )
 }
